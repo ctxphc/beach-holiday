591ff33dce0eda903c8b25343e9a8f8f5d4294f1
Ken Kilgore <kaptkaos@gmail.com>
2016-02-12 10:15:53 -0600

    Added monolog and updated composer

---
+++ includes/Class/Log.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ken_kilgore1
+ * Date: 2/11/2016
+ * Time: 2:43 PM
+ */
+
+namespace CTXPHC\BeachHoliday\Classes;
+
+
+class Log {
+
+}
---
+++ includes/Class/LogWriter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ken_kilgore1
+ * Date: 2/11/2016
+ * Time: 2:39 PM
+ */
+
+namespace CTXPHC\BeachHoliday\Classes;
+
+
+class LogWriter {
+
+}
